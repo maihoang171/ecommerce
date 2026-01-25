c26c660dbbf5981eabb0f3a6acb97f9a4f16d069
Jayce <maihoang171@gmail.com>
2026-01-25 09:03:48 +0700

    delete console.log

--- ecommerce-be/src/controllers/product/product-controller.js
+++ ecommerce-be/src/controllers/product/product-controller.js
@@ -2,7 +2,7 @@ import { getService } from "../../services/product-service.js";
 
 export const getController = async (req, res, next) => {
   const { isSale, keyword, categoryId } = req.query;
-  console.log(categoryId)
+
   try {
     const parsedCategoryId = categoryId ? Number(categoryId) : undefined;
     const products = await getService({
